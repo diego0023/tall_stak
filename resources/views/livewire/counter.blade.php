3b474d7745d1fe8469c8cceea033dd51d9d7303d
Diego G <diegogalan.0023@gmail.com>
2023-07-14 15:28:49 -0600

    cambio de funte

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div x-data="{ open: false }">
-        <button class="rounded-xl bg-blue-400 h-12 w-[100px]"  @click="open = ! open">Toggle Content</button>
+        <button class="rounded-xl bg-blue-400 h-12 w-[100px] font-semibold"  @click="open = ! open">Toggle Content</button>
         <div x-show="open">
             Content...
         </div>
