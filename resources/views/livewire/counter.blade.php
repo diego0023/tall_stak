67ae5fec3b6f37e03cea6d892738441d71da3e18
Diego G <diegogalan.0023@gmail.com>
2023-07-14 15:17:24 -0600

    configuracion del proyecto

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div x-data="{ open: false }">
+        <button class="rounded-xl bg-blue-400 h-12 w-[100px]"  @click="open = ! open">Toggle Content</button>
+        <div x-show="open">
+            Content...
+        </div>
+    </div>
+</div>
